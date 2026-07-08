b329b93f456efbbd3a341f43faa7b11645d8d88a
Mayuresh Jadhav <mayureshj1711@gmail.com>
2026-07-08 10:45:47 +0530

    Fix filename case

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import React, { useState } from "react";
+import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
+import MainBox from "./Components/MainbBox"; // or MainBox if you rename the file
+import Style from "./CSS/App.module.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const App = () => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <>
+      <Navbar toggle={toggle} setToggle={setToggle} />
+      <div className={Style.mainLayout}>
+        <Sidebar toggle={toggle} />
+        <MainBox />
+      </div>
+    </>
+  );
+};
+
+export default App;
